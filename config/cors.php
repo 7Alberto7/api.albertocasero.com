98f6d7709cb3bd2b36fb817a0526086bc4a8658d
Alberto Casero <j.albertocg86@gmail.com>
2020-06-21 15:17:53 +0200

    Solucionar problemas con CORS TG-67

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
-        'https://albertocasero.com/',
+        'https://albertocasero.com',
     ],
 
     'allowed_origins_patterns' => [],
